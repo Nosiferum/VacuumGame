cdcbaa442a92b0e7e7ab1564a59eda342c1a421b
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2023-02-10 15:13:17 +0300

    -Added new AI controller

---
+++ Source/VacuumGame/VacuumAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "VacuumAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VACUUMGAME_API AVacuumAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
